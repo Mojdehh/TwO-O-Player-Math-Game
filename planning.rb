7499d5a1ef446cdfdacf82ca7196a88279821a1c
Mojdeh <mojdeh@Mojdehs-MacBook-Pro.local>
2022-01-03 15:30:00 -0500

    planning approach

---
+++ planning.rb
@@ -0,0 +1,14 @@
+# this class will be initalized with name
+# has a method that deduct life points based on wrong answer
+class Player
+end
+
+
+# this class will ask the question and calculate the answer based on two random numbers 
+class Questions
+end
+
+
+# game logic and loop will happen here
+class Main
+end
