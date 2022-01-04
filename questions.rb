80001fa90ec8583a1b1d68227dff4c59008d8b3e
Mojdeh <mojdeh@Mojdehs-MacBook-Pro.local>
2022-01-03 20:03:18 -0500

    change the name of instance variable

--- questions.rb
+++ questions.rb
@@ -1,9 +1,9 @@
 class Questions
-  attr_accessor :answer, :question
+  attr_accessor :answer, :prompt, :value1, :value2
   def initialize
     @value1 = 1+rand(20)
     @value2 = 1+rand(20)
     @answer = value1 + value2
-    @question = "What does #{value1} plus #{value2} equal?"
+    @prompt = "What does #{@value1} plus #{@value2} equal?"
   end
 end
